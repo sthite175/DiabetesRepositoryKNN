f47db5cf03bcbdcc46c9a2c542a6dbefdc961725
sthite175 <sthite175@gmail.com>
2023-02-11 17:31:32 +0530

    Added interface.py file

---
+++ interface.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request,jsonify
+from utilities import diabetes_prediction
+import pickle
+import config
+
+app=Flask(__name__)
+@app.route("/")
+def app_home():
+    return render_template("index.html")
+
+@app.route("/prediction",methods=["post"])
+def get_prediction():
+    data=request.form
+    pred_output=diabetes_prediction(data)
+
+    #return jsonify({"Result":diabetes})
+    return render_template("index.html",Result=pred_output)
+
+if __name__=="__main__":
+    app.run(debug=True,port=config.PORT,host=config.HOST)
+    
+
+
+
