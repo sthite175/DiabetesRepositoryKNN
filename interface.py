c5047f7fad414e23f9210b1f399ff0150656f6ca
sthite175 <sthite175@gmail.com>
2023-02-11 18:54:33 +0530

    Updated interface code on interface branch

--- interface.py
+++ interface.py
@@ -3,22 +3,21 @@ from utilities import diabetes_prediction
 import pickle
 import config
 
-app=Flask(__name__)
+app=Flask(__name__)    # Home page
 @app.route("/")
 def app_home():
     return render_template("index.html")
 
 @app.route("/prediction",methods=["post"])
 def get_prediction():
+    # It fatches dictionary from the HTML
     data=request.form
     pred_output=diabetes_prediction(data)
 
     #return jsonify({"Result":diabetes})   # It will for postman result for checking app will work or not
-    return render_template("index.html",Result=pred_output)
+    return render_template("index.html",Result=pred_output) # We have to write code in index.html 
 
+# code run the app
 if __name__=="__main__":
     app.run(debug=True,port=config.PORT,host=config.HOST)
     
-
-
-
